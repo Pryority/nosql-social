03cb9a9d3076f55f3010009f5e5257bfcf50792a
pryority <matthewapryor@gmail.com>
2022-06-24 03:35:16 -0400

    initial

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const reactionSchema = require('./schema/reaction');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            user: { type: Schema.Types.ObjectId, ref: 'User' },
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
+
